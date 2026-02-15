a0bc8e0bf41ed72d12006f0df578b82cde227fd2
Cyto-plasm <ruparakshit25@gmail.com>
2026-02-16 00:33:44 +0530

    feature/UC2: Updated banner and added .class to gitignore

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,8 +1,21 @@
-
+/**
+ * OOPSBannerApp
+ * UC2: Render OOPS as Banner using Print Statements
+ *
+ * @author Rupayan Rakshit
+ * @version 2.0
+ */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
-    }
 
+        System.out.println("  *****    " + "  *****    " + "  *******  " + "   *****   ");
+        System.out.println(" *     *   " + " *     *   " + "  *      * " + "  *     *  ");
+        System.out.println("*       *  " + "*       *  " + "  *      * " + " *         ");
+        System.out.println("*       *  " + "*       *  " + "  *******  " + "  *****    ");
+        System.out.println("*       *  " + "*       *  " + "  *        " + "       *    ");
+        System.out.println(" *     *   " + " *     *   " + "  *        " + " *     *   ");
+        System.out.println("  *****    " + "  *****    " + "  *        " + "  *****    ");
+    }
 }
+
